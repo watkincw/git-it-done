152d70c0900be4906e3efb192285e634da66d2bc
Colton <watkincw@gmail.com>
2021-06-25 15:44:33 -0500

    capturing data returned from the api and updated request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-const getuserRepos = function() { 
-    fetch("https://api.github.com/users/octocat/repos");
-}
+const getuserRepos = function(user) {
+    // format the github apu url
+    const apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+    // make a request to the url
+    fetch(apiUrl).then(function(response) { 
+        response.json().then(function(data) { 
+            console.log(data);
+        });
+    });
+};
 
-getuserRepos();
+getuserRepos("watkincw");
