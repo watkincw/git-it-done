ea7f95738c864c48cd9af52d88b497b79f8d9555
Colton <watkincw@gmail.com>
2021-06-25 17:16:38 -0500

    created functionality to search for github user, then display their github repos on the page. Also to display if there are open issues or not in each repo.-.then added error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,90 @@
-const getuserRepos = function(user) {
+const userFormEl = document.querySelector("#user-form");
+const nameInputEl = document.querySelector("#username");
+const repoContainerEl = document.querySelector("#repos-container");
+const repoSearchTerm = document.querySelector("#repo-search-term");
+
+const formSumbitHandler = function(event) { 
+    event.preventDefault();
+
+    // get value from input element
+    let username = nameInputEl.value.trim();
+
+    if (username) { 
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else { 
+        alert("Please enter a GitHub username");
+    }
+}
+
+const getUserRepos = function(user) {
     // format the github apu url
     const apiUrl = "https://api.github.com/users/" + user + "/repos";
     
     // make a request to the url
     fetch(apiUrl).then(function(response) { 
-        response.json().then(function(data) { 
-            console.log(data);
-        });
+        if (response.ok) { 
+            response.json().then(function(data) { 
+                displayReops(data, user);
+            });
+        } else { 
+            alert("Error: GitHub User Not Found");
+        }
+    })
+    .catch(function(error) { 
+    // this `.catch()` is getting chained to the end of the `.then()`
+    alert("Unable to connect to GitHub");
     });
 };
 
-getuserRepos("watkincw");
+const displayReops = function(repos, searchTerm) { 
+    console.log(repos);
+    console.log(searchTerm);
+
+    // check if api returened any repos
+    if (repos.length === 0) { 
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    // loop over repos
+    for (let i = 0; i < repos.length; i++) { 
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repositiry name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) { 
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else { 
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+        // append to container
+        repoEl.appendChild(statusEl);
+
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+}
+
+userFormEl.addEventListener("submit", formSumbitHandler);
